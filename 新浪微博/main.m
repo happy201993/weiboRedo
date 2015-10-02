d77b6ac036ccdcfb814c682236f669ad909c3e68
yangdaye <15757176639@163.com>
2015-10-02 22:03:37 +0800

    Initial Commit

---
+++ 新浪微博/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  新浪微博
+//
+//  Created by 杨少伟 on 15/10/2.
+//  Copyright © 2015年 YCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
